666ab1c40e8bd10ae318e40ef50bce85a2742ea1
ua2701 <usamaahamed.1si20ec117@gmail.com>
2025-05-29 17:04:38 +0530

    Fix: added Flask host and port binding for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from playwright_script import run_login_if_authorized
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +10,8 @@ def login_bank(username):
         return run_login_if_authorized(username)
     else:
         return "Unauthorized username", 403
+
+#Required block to run properly on Render
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
